44eb3af734b96487e6593bc62772aa26077c0d90
anushaath <anushaath.p@gmail.com>
2016-11-14 22:35:12 +0530

    Update game.py

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ while True:
 			break
 	while True:
 		x=int(input("\n Enter the size of the word: "))
-		if x>=1 && x<=10:
+		if x>=1 and x<=10:
 			break
 		
 	word= id_generator(x)
@@ -33,15 +33,25 @@ def guesses(wrd,lmt):
 		guess=input("\n Guess a string in CAPS: ")
 		no_of_guess= no_of_guess+1
 		corr_pos,wrg_pos=0
-		wrg_letter
+		wrg_letter=0
+		#Game Logic
+		for x in guess:
+			if x in wrd:
+				if guess.index(x) == wrd.index(x):
+					corr_pos= corr_pos +1
+				else :
+					wrg_pos=wrg_pos+1
+		#--------------
+		print("The number of characters that are correct but are in the wrong place is "+wrg_pos)
+		print("The number of characters that are correct and are in the correct place is "+corr_pos)
 		
 		
 		
 		
-		if no_of_guess>= lmt || found==1:
+		if no_of_guess>= lmt or found==1:
 			break
-	if found == 1 
+	if found == 1:
 		print("\n Congrats! ")
-	else
+	else:
 		print("\n Hard Luck!")
 	
